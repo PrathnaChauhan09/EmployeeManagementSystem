ea20d5f4c9b7b2fa0dc917d457a82e0677ffd9d8
PrathnaChauhan09 <prathna@promactinfo.com>
2020-10-29 18:12:59 +0530

    Changes

--- Data/EmployeeContext.cs
+++ Data/EmployeeContext.cs
@@ -2,10 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EmployeeManagementSystem.Models;
 
 namespace EmployeeManagementSystem.Data
 {
-    public class EmployeeContext
+    public class EmployeeContext : DbContext
     {
+        public EmployeeContext(DbContextOptions<EmployeeContext> options) :
+            base(options)
+        { }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Entity<Department>().ToTable("Department");
+        //    modelBuilder.Entity<Employee>().ToTable("Employee");
+        //}
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EmployeeManagementSystem.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -24,6 +25,9 @@ namespace EmployeeManagementSystem
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<EmployeeContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("EmployeeConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
