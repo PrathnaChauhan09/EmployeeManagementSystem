f1c71808a436957fb27bd59e4ea32ae284efd391
PrathnaChauhan09 <prathna@promactinfo.com>
2020-10-29 17:57:48 +0530

    New Repository

---
+++ Data/EmployeeContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementSystem.Data
+{
+    public class EmployeeContext
+    {
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementSystem.Models
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string Qualification { get; set; }
+        public string ContactNo { get; set; }
+
+        public Department Department { get; set; }
+
+    }
+}
